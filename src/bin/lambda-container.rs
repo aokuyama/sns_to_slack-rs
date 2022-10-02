b281f4f4d18a31fdc021378ebfd419bb2174d3d9
aokuyama <ao980s@gmail.com>
2022-10-02 15:54:28 +0900

    add lambda handler

---
+++ src/bin/lambda-container.rs
@@ -0,0 +1,12 @@
+use sns_to_slack::lambda_handler;
+
+use lambda_runtime::{
+    Error as LambdaError,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), LambdaError> {
+    let handler = lambda_runtime::service_fn(lambda_handler);
+    let r = lambda_runtime::run(handler).await?;
+    Ok(r)
+}
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use serde::Serialize;
+use lambda_runtime::{LambdaEvent, Error};
+use aws_lambda_events::event::sns::SnsEvent;
+mod slack;
+
+#[derive(Debug, Serialize)]
+pub struct LambdaResult {
+}
+
+pub async fn lambda_handler(event: LambdaEvent<serde_json::Value>)  -> Result<LambdaResult, Error> {
+    let sns: SnsEvent = serde_json::from_str(&event.payload.to_string()).unwrap();
+    handle_sns(sns).await
+}
+
+async fn handle_sns(e: SnsEvent) -> Result<LambdaResult, Error> {
+    let client = slack::Client::new();
+    for record in e.records {
+        let message = record.sns.message;
+        let channel = match record.sns.message_attributes.get("channel") {
+            Some(attr) => Some(&attr.value),
+            None => None,
+        };
+        let app_token = match record.sns.message_attributes.get("app_token") {
+            Some(attr) => Some(&attr.value),
+            None => None,
+        };
+        client.send(&message, channel, app_token).await;
+    }
+    Ok(LambdaResult{})
+}
