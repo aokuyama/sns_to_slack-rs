f23329920e9082ae3b320c46abd6f0ebe2e17149
aokuyama <ao980s@gmail.com>
2022-10-02 09:25:33 +0900

    test send message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let text = "test message.";
+    let token = "xoxb-xxxxxx";
+    let channel = "C0XXXXXX";
+
+    let form = reqwest::multipart::Form::new()
+    .text("channel", channel)
+    .text("text", text);
+
+    let client = reqwest::Client::new();
+    let res = client.post("https://slack.com/api/chat.postMessage")
+    .header(reqwest::header::AUTHORIZATION, String::from("Bearer ") + token)
+    .multipart(form)
+    .send()
+    .await;
+
+    let payload = res.expect("failed to get response")
+    .text()
+    .await
+    .expect("failed to get payload");
+    println!("{}", payload);
 }
